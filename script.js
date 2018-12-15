81bfdf1ec9a5a611e4c330d994554a9d9cb7e24a
Sho Yashiro <shirotani@seginus.jp>
2018-12-15 10:26:22 +0900

    フェードモバイル対応
    クリックするとフェードとグルする形式に。
    
    フェードのスピードを変数化。

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ $(document).ready(()=>{
 	
 	let fadeSpeed=300;
 	
-	$(".card").on('mouseenter',()=>{
+	let $card=$(".card")
+	
+	$card.on('click',()=>{$(event.currentTarget).children(".card-img-overlay").stop(true).fadeToggle(fadeSpeed)});
+	
+	$card.on('mouseenter',()=>{
 		let $image=$(event.currentTarget);
 		$image.children(".card-img-overlay").stop(true).fadeIn(fadeSpeed);
 	}).on('mouseleave',()=>{
